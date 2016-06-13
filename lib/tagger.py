afaeba7b87c7d59ccedcfeadea40179133af6cd8
cihang.jin <healthai@qq.com>
2016-06-13 12:42:19 +0800

    add basic_struct method

--- lib/tagger.py
+++ lib/tagger.py
@@ -11,6 +11,11 @@ from entity_tag import *
 from entity_dict import *
 from emr_preprocessor import EMRPreproc
 import traceback
+import gevent
+import time
+from gevent import getcurrent
+
+#from gevent import monkey; monkey.patch_thread()
 
 class Tagger(object):
     def __init__(self):
@@ -19,17 +24,24 @@ class Tagger(object):
         self.emr_preproc.load_messy_code("../mining/entity_tag/data/messy_code.txt")
         edict = EntityDict("symp")
         edict.load_file("../mining/entity_tag/data/class_term.dict.gbk")
-        patternList = Pattern().getPattern_bak()
+        patternList = Pattern().getPattern()
         self.tagger = EntityTagger(edict, patternList, "../mining/entity_tag/dict/wordseg_dict/")
 
     def tag(self, txt, mode):
+        print 'calling tag ', id(getcurrent())
         return self.tagger.tag(txt, mode)
+
+    def basic_struct(self, content):
+        print 'calling basic_struct', id(getcurrent())
+        return self.emr_preproc.basic_struct(content)
 
 if __name__ == "__main__":
     pool_size = 6
     server = zerorpc.Server(Tagger(), pool_size = pool_size)
+    #server = zerorpc.Server(Tagger())
     server.bind("tcp://0.0.0.0:9999")
     print 'tagger service start....'
     server.run()
     server.close()
+
 
